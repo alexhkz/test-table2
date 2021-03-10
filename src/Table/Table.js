f43aabc6fd13e3af914ddaa7d1fe866eb8786036
alexhkz <alexwebcode@gmail.com>
2021-03-10 18:37:00 +0300

    add sortData

--- src/Table/Table.js
+++ src/Table/Table.js
@@ -1,26 +1,42 @@
-import React from 'react';
+import React, {useState} from 'react';
 import ArrowUp from '../svg/ArrowUp';
 import ArrowDown from '../svg/ArrowDown';
 
 const Table = ({sortData, contactData, directionSort}) => {
+
+	const [fieldData, setFieldData] = useState('');
+	const Arrow = () => {
+		return (
+			directionSort ? <ArrowDown/> : <ArrowUp/>
+		)
+	}
+
+	const fieldSortData = (field) => {
+		return (
+			sortData(field),
+			console.log(field),
+			setFieldData(field)
+		)
+	}
+
 	return (
 		<table className="table">
 				<thead>
 					<tr>
-						<th onClick={ () => {sortData('id')} }>
-							Id {directionSort ? <ArrowDown/> : <ArrowUp/>}
+						<th onClick={ () => {fieldSortData('id')} }>
+							Id {fieldData === 'id' ? <Arrow/> : null}
 							</th>
-						<th onClick={ () => {sortData('firstName')} }>
-							FirstName {directionSort ? <ArrowDown/> : <ArrowUp/>}
+						<th onClick={ () => {fieldSortData('firstName')} }>
+							FirstName {fieldData === 'firstName' ? <Arrow/> : null}
 							</th>
-						<th onClick={ () => {sortData('lastName')} }>
-							LastName {directionSort ? <ArrowDown/> : <ArrowUp/>}
+						<th onClick={ () => {fieldSortData('lastName')} }>
+							LastName {fieldData === 'lastName' ? <Arrow/> : null}
 							</th>
-						<th onClick={ () => {sortData('email')} }>
-							Email {directionSort ? <ArrowDown/> : <ArrowUp/>}
+						<th onClick={ () => {fieldSortData('email')} }>
+							Email {fieldData === 'email' ? <Arrow/> : null}
 							</th>
-						<th onClick={ () => {sortData('phone')} }>
-							Phone {directionSort ? <ArrowDown/> : <ArrowUp/>}
+						<th onClick={ () => {fieldSortData('phone')} }>
+							Phone {fieldData === 'phone' ? <Arrow/> : null}
 							</th>
 					</tr>
 				</thead>
